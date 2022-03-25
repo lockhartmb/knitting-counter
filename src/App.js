8486259257952a8331044974cc648d8bd5b5a7b2
Morgan <46503510+lockhartmb@users.noreply.github.com>
2022-03-25 18:07:05 -0400

    ✨ knew knitting row counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const handleReset = () => {
+    document.getElementById('form').reset();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Knitting counter</h1>
+        <form action='' id='form'>
+          <label htmlFor='row1'>
+            <input id='row1' name='row1' type='checkbox' />
+            Row 1
+          </label>
+          <label htmlFor='row2'>
+            <input id='row2' name='row2' type='checkbox' />
+            Row 2
+          </label>
+          <label htmlFor='row3'>
+            <input id='row3' name='row3' type='checkbox' />
+            Row 3
+          </label>
+          <label htmlFor='row4'>
+            <input id='row4' name='row4' type='checkbox' />
+            Row 4
+          </label>
+          <br />
+          <button type='button' onClick={handleReset}>
+            Reset
+          </button>
+        </form>
       </header>
     </div>
   );
-}
+};
 
 export default App;
